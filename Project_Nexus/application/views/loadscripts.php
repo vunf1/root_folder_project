e398e91fb7ca4ef5904ce4a96c747832ecc6cf79
vunf1 <jokass.workplace@gmail.com>
2019-04-11 17:14:20 +0100

    server fix 14

--- Project_Nexus/application/views/loadscripts.php
+++ Project_Nexus/application/views/loadscripts.php
@@ -2,10 +2,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-  <link rel="icon" href="<?=base_url()?>favicon.ico" type="image/gif">
+  <link rel="icon" href="/favicon.ico" type="image/gif">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+  <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 <!-- 
 
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -14,35 +14,34 @@
 
 
  -->
-  <script type='text/javascript' src='<?php ?>assets/js/jquery-ui.js'></script>
+<script type='text/javascript' src='<?php ?>assets/js/jquery-ui.js'></script>
   
-  <script type="text/javascript" src="<?php ?>assets/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="<?php ?>assets/js/jquery-1.9.1.min.js"></script>
    
- <script type="text/javascript" src="<?php ?>assets/js/jquery.session.js"></script>
+<script type="text/javascript" src="<?php ?>assets/js/jquery.session.js"></script>
 
  
  
- <link rel="stylesheet" href="<?php ?>assets/alertify/css/alertify.css">
+<link rel="stylesheet" href="<?php ?>assets/alertify/css/alertify.css">
 
- <script type="text/javascript" src="<?php ?>assets/alertify/alertify.js"></script>
+<script type="text/javascript" src="<?php ?>assets/alertify/alertify.js"></script>
  
   
- <link rel="stylesheet" href="<?php ?>assets/css/bootstrap-select.css">
- <script type="text/javascript" src="<?php ?>assets/js/bootstrap-select.js"></script>
+<link rel="stylesheet" href="<?php ?>assets/css/bootstrap-select.css">
+<script type="text/javascript" src="<?php ?>assets/js/bootstrap-select.js"></script>
  
  
  
- <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
  
  
- <script type="text/css" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2708/bootstrap-datetimepicker.min.css"></script>
+<script type="text/css" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2708/bootstrap-datetimepicker.min.css"></script>
     
  
  
 
   
-  <!--<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css">
-  -->
+<!--<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css">  -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
