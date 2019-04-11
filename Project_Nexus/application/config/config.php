46d0302596e247a4bcaeed6985ed10544d63b8f4
vunf1 <jokass.workplace@gmail.com>
2019-04-11 15:40:16 +0100

    base URL cov.domains host server

--- Project_Nexus/application/config/config.php
+++ Project_Nexus/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'vunf1.coventry.domains';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
