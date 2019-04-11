6487f4f2bb3396228ae7c54bd7fc75d8a814db39
vunf1 <jokass.workplace@gmail.com>
2019-04-11 22:47:40 +0100

    fixing

--- Project_Nexus/application/models/Tionmodel.php
+++ Project_Nexus/application/models/Tionmodel.php
@@ -645,7 +645,7 @@ public function get_tables_name() {
         array_push($ok,$table);
         
     }*/
-    return json_encode($tables->result(), JSON_FORCE_OBJECT);
+    return json_encode($tables->result(), false);
     
  }
  
