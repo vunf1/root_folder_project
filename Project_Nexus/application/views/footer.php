9724347fdcf0ffe81658495b62c690f888a8e224
vunf1 <jokass.workplace@gmail.com>
2019-04-11 16:43:34 +0100

    server fix 10

--- Project_Nexus/application/views/footer.php
+++ Project_Nexus/application/views/footer.php
@@ -21,8 +21,8 @@ img{
     
 </style>
 <div id="footer_" class="footer">
-    <img title="Running" src=/assets/img/00.gif"><span class="copyleft">&copy;</span>Copyleft 2017  <strong>João Maia</strong>.OpenSource 
-    <label id="opensource-logo"><img title="Running" src=/assets/img/opensource.png"></label> 
+    <img title="Running" src=/root_folder_project/Project_Nexus/assets/img/00.gif"><span class="copyleft">&copy;</span>Copyleft 2017  <strong>João Maia</strong>.OpenSource 
+    <label id="opensource-logo"><img title="Running" src=/root_folder_project/Project_Nexus/assets/img/opensource.png"></label> 
     
     
     
