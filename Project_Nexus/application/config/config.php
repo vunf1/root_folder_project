1987cab168bd9c5e283a709a0184962bd40eb765
vunf1 <jokass.workplace@gmail.com>
2019-04-11 18:08:05 +0100

    server form - login

--- Project_Nexus/application/config/config.php
+++ Project_Nexus/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'root_folder_project/Project_Nexus';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
