edb5a850ee8d4baedd4efb7a20a556b2280f183e
Deoiveij <deoiveij@uni.coventry.ac.uk>
2019-07-10 17:35:17 +0000

    Portfolio- add securety parameters, improvement new root folder , fix and add text to portfolio

--- root.php
+++ root.php
@@ -8,6 +8,7 @@
         
         <title>Root page</title>
     
+        <script src="https://vunf1.coventry.domains/js/resume.min.js"></script>
         <link href="https://vunf1.coventry.domains/root_folder_project/custom.css" rel="stylesheet">
          <script>
            window.history.pushState("", "", '/root');
@@ -45,9 +46,9 @@
         <?php//  Body Head ?>
         
 
-
-            <span class="badge badge-primary">Web-Based SSL Certificated </span>
+            <span class="badge badge-primary" onclick="triggerDiv('Web-Based')">Web-Based SSL Certificated </span>
             <br>
+            <div id="Web-Based" style="display:none;">
             <a target="_blank" style="color: darkgrey;" class="btn " id="Project_Nexus" href="https://vunf1.coventry.domains/root_folder_project/Project_Nexus/">Project Nexus <span class="badge badge-light">2017</span></a>
             <br>
             
@@ -56,17 +57,22 @@
             
             <a href="https://vunf1.coventry.domains/real_world_project/RWP_releaseVersion.apk"><span id="RWP_releaseVersion.apk"  class="badge badge-warnning">.APK</span></a><a target="_blank" style="color: darkgrey;" class="btn " id="real_world_project" href="https://vunf1.coventry.domains/real_world_project"  >Navigation Campus <span class="badge badge-light">2019</span></a>
             <br>
+            </div>
 
             
-            <span class="badge badge-info">vb.NET</span>
+            <span class="badge badge-info" onclick="triggerDiv('vb.NET')">vb.NET</span>
             <br>
+            <div id="vb.NET" style="display:none;">
 
-            <a style="color: darkgrey;" class="btn " id="vbNet_Software_2016.zip" href="https://vunf1.coventry.domains/vbNet_Software_2016.zip"   >Report <span class="badge badge-light">2016</span></a>
+              <a style="color: darkgrey;" class="btn " id="vbNet_Software_2016.zip" href="https://vunf1.coventry.domains/vbNet_Software_2016.zip"   >Report <span class="badge badge-light">2016</span></a>
             <br>
+            </div>
             
             
-            <span class="badge badge-danger">Java</span>
+            <span class="badge badge-danger"  onclick="triggerDiv('Java')">Java</span>
             <br>
+            
+            <div id="Java" style="display:none;">
             <a style="color: darkgrey;" class="btn " id="JARcase_Study_Coursework.zip" href="https://vunf1.coventry.domains/JARcase_Study_Coursework.zip"   >Gym Management <span class="badge badge-light">2019</span></a>
             <br>
 
@@ -82,48 +88,16 @@
         </div>
         
         <script >
-          var d = new Date();
-          //var strDate = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
-          var strDate = d.getFullYear();
-          $("#datt").html(strDate);
-          
-                function onRootOFF(folderName){
-                  if(folderName=="Project_Nexus" ){
-                    alertify.message('Loading...'+folderName+' ');
-                    
-                    window.location.replace("https://vunf1.coventry.domains/root_folder_project/"+folderName);
+      $(document).ready(function () {
+       // $("#loading2").css("display","none");
+        $("#footer_").css("display","block");
 
-                  }else if(folderName=="RWP_releaseVersion.apk"){
-                    alertify.message('Loading... APK');
-                    window.location.replace("https://vunf1.coventry.domains/real_world_project/"+folderName);
-                  }else{
-                    if(folderName=="JARcase_Study_Coursework.zip"){
-                      alertify.message('Loading... JAR');
-                      window.location.replace("https://vunf1.coventry.domains/"+folderName);
+        $(".container").css("display","block");
 
-                    }else if(folderName=="vbNet_Software_2016.zip"){
-                      alertify.message('Loading... vb.NET');
-                      window.location.replace("https://vunf1.coventry.domains/"+folderName);
 
-                    }else{
-                      alertify.message('Loading...'+folderName);
-                      window.location.replace("https://vunf1.coventry.domains/"+folderName);
 
-                    }
-                  }
-                      
-                };
             
-                $(document).ready(function () {
-                  $("#loading2").css("display","none");
-                  $("#footer_").css("display","block");
-
-                  $(".container").css("display","block");
-
-
-
-                      
-                });
+      });
                 </script>
         
         </body>
